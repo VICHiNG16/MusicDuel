7b0af48531f57d3b08f8e50c1a8974636989a11b
VICHiNG16 <caluianu.andrei123@gmail.com>
2025-12-10 21:10:12 +0200

    Stable fixed version: Release APK build and Search functionality restored

--- utils/firebaseConfig.ts
+++ utils/firebaseConfig.ts
@@ -5,7 +5,6 @@ import { initializeAuth, getReactNativePersistence, getAuth } from "firebase/aut
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Platform } from "react-native";
 
-
 const firebaseConfig = {
     apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
     authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
@@ -14,7 +13,7 @@ const firebaseConfig = {
     storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
-    measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID
+    measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
