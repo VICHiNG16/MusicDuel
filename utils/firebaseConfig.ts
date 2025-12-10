014b45319cc539b13bd99f9f19662c5b976cca5c
VICHiNG16 <caluianu.andrei123@gmail.com>
2025-12-10 17:11:30 +0200

    security: remove hardcoded api key and use env vars

--- utils/firebaseConfig.ts
+++ utils/firebaseConfig.ts
@@ -7,14 +7,14 @@ import { Platform } from "react-native";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDU68yyfZLnG7LiPZaGPfbBajI1HYEkCWA",
-    authDomain: "music-game-304b9.firebaseapp.com",
-    databaseURL: "https://music-game-304b9-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "music-game-304b9",
-    storageBucket: "music-game-304b9.firebasestorage.app",
-    messagingSenderId: "701599299100",
-    appId: "1:701599299100:web:e9d08b1c2378be2c93d1d9",
-    measurementId: "G-9NMHVWZEGL"
+    apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.EXPO_PUBLIC_FIREBASE_DATABASE_URL,
+    projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
